300e17e5b30be3dd3f13de7723a50c8c0272a1c6
APS4087 <aungpyaesoe.bgs@Gmail.com>
2024-08-16 19:07:10 +0800

    finish register page UI

--- app/auth/register/index.jsx
+++ app/auth/register/index.jsx
@@ -1,9 +1,17 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import React from 'react';
+import { useRouter } from 'expo-router';
+
+
 
 export default function RegisterScreen() {
+
+  const router = useRouter();
   return (
     <View style={styles.container}>
+      <TouchableOpacity style={styles.logo} onPress={() => router.push('/auth')}>
+        <Image source={require('../../../assets/Icons/backArrow.png')} style={styles.arrow} />
+      </TouchableOpacity>
       <Text style={styles.title}>Welcome!! Register to get started</Text>
       <TextInput
         style={styles.input}
@@ -29,6 +37,11 @@ export default function RegisterScreen() {
       <TouchableOpacity style={styles.registerButton}>
         <Text style={styles.registerButtonText}>Register</Text>
       </TouchableOpacity>
+      <TouchableOpacity style={styles.loginLinkButton} onPress={()=>router.push('/auth/login')}>
+        <Text style={styles.alreadyHaveAccountText}>Already have an account ?
+        <Text style={styles.loginLinkButtonText}>   Login Now</Text>
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -40,11 +53,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
     padding: 16,
   },
+  logo: {
+    height: 30,
+    width: 30,
+    marginLeft: 20,
+    marginTop: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  arrow: {
+    height: '100%',
+    width: '100%',
+    resizeMode: 'contain',
+  },
   title: {
-    marginTop: '20%',
-    fontSize: 30,
+    marginTop: '10%',
+    fontSize: 38,
     fontWeight: 'bold',
-    marginBottom: 32,
+    marginBottom: 45,
     marginLeft: 20,
   },
   input: {
@@ -57,7 +83,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginLeft: 20,
     marginBottom: 10,
-    marginTop: 20,
+    marginTop: 10,
     backgroundColor: '#F7F8F9',
   },
   registerButton: {
@@ -66,13 +92,30 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     width: '80%',
     alignItems: 'center',
-    marginTop: 60,
+    marginTop: 40,
     marginLeft: 35,
     height: 50,
   },
   registerButtonText: {
     color: '#ffffff',
     fontSize: 16,
+    fontWeight: 'bold',
+  },
+  alreadyHaveAccountText: {
+    fontSize: 10,
+    width: '100%',
+    // borderWidth: 1,
+  },
+  loginLinkButton: {
+    marginTop: 10,
+    width: '50%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+
+  },
+  loginLinkButtonText: {
+    color: '#0000EE',
+    fontSize: 12,
     fontWeight: 'bold',
   },
 });
