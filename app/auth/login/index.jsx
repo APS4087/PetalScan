f00482f05f6f17b5fa9248ab60cab99911e45784
APS4087 <aungpyaesoe.bgs@Gmail.com>
2024-08-16 15:15:22 +0800

    register link to login page

--- app/auth/login/index.jsx
+++ app/auth/login/index.jsx
@@ -1,9 +1,15 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import React from 'react';
+import { useRouter } from 'expo-router';
 
 export default function LoginScreen() {
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
+      <TouchableOpacity style={styles.logo} onPress={() => router.push('/auth')}>
+        <Image source={require('../../../assets/Icons/backArrow.png')} style={styles.arrow} />
+      </TouchableOpacity>
       <Text style={styles.title}>Sign in to your Account</Text>
       <TextInput
         style={styles.input}
@@ -13,12 +19,22 @@ export default function LoginScreen() {
       />
       <TextInput
         style={styles.input}
-        placeholder="Email your password"
+        placeholder="Enter your password"
         secureTextEntry
       />
+      <TouchableOpacity style={styles.forgotPasswordButton}>
+        <Text style={styles.forgotPasswordButtonText}>Forgot Password ?</Text>
+      </TouchableOpacity>
       <TouchableOpacity style={styles.loginButton}>
         <Text style={styles.loginButtonText}>Login</Text>
       </TouchableOpacity>
+      <TouchableOpacity style={styles.registerLinkButton} onPress={()=>router.push('/auth/register')}>
+        <Text style={styles.dontHaveAccountText}>Don't have an account ?
+        <Text style={styles.registerLinkButtonText}>   Register</Text>
+        </Text>
+      </TouchableOpacity>
+      
+      
     </View>
   );
 }
@@ -26,15 +42,28 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'left', 
+    alignItems: 'flex-start', 
     backgroundColor: '#ffffff',
     padding: 16,
   },
+  logo: {
+    height: 30,
+    width: 30,
+    marginLeft: 20,
+    marginTop: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  arrow: {
+    height: '100%',
+    width: '100%',
+    resizeMode: 'contain',
+  },
   title: {
-    marginTop: '30%',
+    marginTop: '20%',
     fontSize: 40,
     fontWeight: 'bold',
-    marginBottom: 32,
+    marginBottom: 50,
     marginLeft: 20,
   },
   input: {
@@ -43,8 +72,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#cccccc',
     borderRadius: 8,
-    alignItems: 'center', 
-    justifyContent: 'center',
     marginLeft: 20,
     marginBottom: 10,
     marginTop: 20,
@@ -56,13 +83,43 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     width: '80%',
     alignItems: 'center',
-    marginTop: 60,
+    marginTop: 35,
     marginLeft: 35,
     height: 50,
+    marginBottom: 10,
   },
   loginButtonText: {
     color: '#ffffff',
     fontSize: 16,
+    fontWeight: 'bold',
+
+  },
+  forgotPasswordButton: {
+    //borderWidth: 1,
+    width: '30%',
+    alignSelf: 'flex-end',
+    marginRight: 20,
+
+  },
+  forgotPasswordButtonText: {
+    color: '#0000EE',
+    fontSize: 12,
+  },
+  dontHaveAccountText: {
+    fontSize: 10,
+    width: '100%',
+    // borderWidth: 1,
+  },
+  registerLinkButton: {
+    //borderWidth: 1,
+    width: '50%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+
+  },
+  registerLinkButtonText: {
+    color: '#0000EE',
+    fontSize: 11,
     fontWeight: 'bold',
   },
 });
